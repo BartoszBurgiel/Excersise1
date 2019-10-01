e74417338a9d664d19a6b7f75013840f3f9272ab
Bartosz <bartek.burgiel@hotmail.com>
2019-10-01 09:10:42 +0200

    Fix log messages

--- person.go
+++ person.go
@@ -16,55 +16,48 @@ type Person struct {
 
 // GrabGlasses simulates a person grabbing their glasses
 func (p Person) GrabGlasses() {
-	fmt.Println(p.Name, " grabs the glasses")
-
 	dur := waitRandomTime(5, 10)
 	p.GettingReadyTime += dur
 
-	fmt.Printf("It took %s %d seconds\n\n", p.Name, dur)
-
+	fmt.Printf("%s grabs the glasses \n It took %s %d seconds\n\n", p.Name, p.Name, dur)
 }
 
 // TideShoes simulates a person tiding their shoes
 func (p Person) TideShoes() {
-	fmt.Println(p.Name, " tides the shoes")
 
 	dur := waitRandomTime(20, 35)
 	p.GettingReadyTime += dur
 
-	fmt.Printf("It took %s %d seconds\n\n", p.Name, dur)
+	fmt.Printf("%s tides the shoes \n It took %s %d seconds\n\n", p.Name, p.Name, dur)
 
 }
 
 // CloseWindow simulates a person closing the windows
 func (p Person) CloseWindow() {
-	fmt.Println(p.Name, " closes the window")
 
 	dur := waitRandomTime(2, 5)
 	p.GettingReadyTime += dur
 
-	fmt.Printf("It took %s %d seconds\n\n", p.Name, dur)
+	fmt.Printf("%s closes the window \n It took %s %d seconds\n\n", p.Name, p.Name, dur)
 
 }
 
 // TurnOffTheFan simulates a person turning off the ceiling fan
 func (p Person) TurnOffTheFan() {
-	fmt.Println(p.Name, " turns off the fan")
 
 	dur := waitRandomTime(3, 6)
 	p.GettingReadyTime += dur
 
-	fmt.Printf("It took %s %d seconds\n\n", p.Name, dur)
+	fmt.Printf("%s turns off the fan \n It took %s %d seconds\n\n", p.Name, p.Name, dur)
 
 }
 
 // PocketBelongings simulates a person pocketing their belongings
 func (p Person) PocketBelongings() {
-	fmt.Println(p.Name, " pockets the belongings")
 
 	dur := waitRandomTime(5, 40)
 	p.GettingReadyTime += dur
 
-	fmt.Printf("It took %s %d seconds\n\n", p.Name, dur)
+	fmt.Printf("%s pockets the belongings \n It took %s %d seconds\n\n", p.Name, p.Name, dur)
 
 }

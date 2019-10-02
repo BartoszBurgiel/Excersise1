e945780f65b5178b64b63abcfbf52994526532b1
Bartosz Burgiel <bartek.burgiel@hotmail.com>
2019-10-02 15:26:13 +0200

    Add varying seed for random time waiter

--- person/wait.go
+++ person/wait.go
@@ -7,6 +7,10 @@ import (
 
 // waitRandomTime waits a random amout of time (ms) and returns the duration
 func waitRandomTime(min, max int) int {
+
+	// New seed
+	rand.Seed(time.Now().UnixNano())
+
 	// Amout of time
 	ms := rand.Intn(max-min) + min
 
